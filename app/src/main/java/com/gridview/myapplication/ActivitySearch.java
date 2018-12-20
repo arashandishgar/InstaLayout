b39701b86edb535070fced4325cf754179ed3c18
arashandishgar <arashandishgar1@gmail.com>
2018-12-20 12:50:36 +0330

    finish

--- app/src/main/java/com/gridview/myapplication/ActivitySearch.java
+++ app/src/main/java/com/gridview/myapplication/ActivitySearch.java
@@ -1,24 +1,19 @@
 package com.gridview.myapplication;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class ActivitySearch extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    setContentView(R.layout.activity_saearch);
     setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
     final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recy);
     final RecyclerView recyclerView0 = (RecyclerView) findViewById(R.id.recy0);
@@ -54,30 +49,7 @@ public class MainActivity extends AppCompatActivity {
     integers.add(R.drawable.image12);
     integers.add(R.drawable.image13);
     integers.add(R.drawable.image14);
-    recyclerView.setAdapter(new Adapter(MainActivity.this,integers));
-    recyclerView0.setAdapter(new Adapter(MainActivity.this, integers));
-  }
-
-  void intentMessageTelegram(String msg, Uri uri) {
-    final String appName = "org.telegram.messenger";
-    final boolean isAppInstalled = isAppAvailable(this.getApplicationContext(), appName);
-    if (isAppInstalled) {
-      Intent myIntent = new Intent(Intent.ACTION_SEND);
-      myIntent.setType("text/plain");
-      myIntent.putExtra(Intent.EXTRA_TEXT, msg);//
-      startActivity(Intent.createChooser(myIntent, "Share with"));
-    } else {
-      Toast.makeText(this, "Telegram not Installed", Toast.LENGTH_SHORT).show();
-    }
-  }
-
-  public static boolean isAppAvailable(Context context, String appName) {
-    PackageManager pm = context.getPackageManager();
-    try {
-      pm.getPackageInfo(appName, PackageManager.GET_ACTIVITIES);
-      return true;
-    } catch (PackageManager.NameNotFoundException e) {
-      return false;
-    }
+    recyclerView.setAdapter(new Adapter(ActivitySearch.this,integers));
+    recyclerView0.setAdapter(new Adapter(ActivitySearch.this, integers));
   }
 }
